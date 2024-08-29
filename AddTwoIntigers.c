7b2f720129ad0b4e7de93a5b8daf1b8b0737fadd
cosine2A <109309785+cosine2A@users.noreply.github.com>
2024-08-29 21:49:49 +0530

    Create AddTwoIntigers.c

---
+++ AddTwoIntigers.c
@@ -0,0 +1,12 @@
+//Write a C Program to Add Two Intigers.
+#include <stdio.h>
+int main(){
+    int in1;
+    int in2;
+    printf("Enter a Intiger1:");
+    scanf("%d",&in1);
+    printf("Enter a Intiger2:");
+    scanf("%d",&in2);
+    printf("\nThe Addition of the Intigers is %d",in1+in2);
+    return 0;
+}
