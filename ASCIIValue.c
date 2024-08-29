11e37d7174e46927ebd5894dadcd959c514d153c
cosine2A <109309785+cosine2A@users.noreply.github.com>
2024-08-29 21:50:58 +0530

    Create ASCIIValue.c

---
+++ ASCIIValue.c
@@ -0,0 +1,11 @@
+// Program To Find the ASCII Value of a Charactor.
+
+#include <stdio.h> 
+int main()
+
+{
+  char c;
+  scanf("%c",&c);
+  printf("The ASCII Value of given Charactor is");
+  printf("%d",c);   // WHen %d is used the output in the ascii value of given input
+}
